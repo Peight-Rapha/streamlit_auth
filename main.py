5d1c72da6c0ed38ee5e0d4f284b4c983f190fdef
Raphael Rodrigues <80860792+Peight-Rapha@users.noreply.github.com>
2025-05-29 22:14:56 -0300

    teste

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import streamlit_authenticator as stauth
 print('teste')
 print('teste')
 print('teste')
+print('teste')
+
 
 # escreva algo para mostrar no streamlit
 st.title('Teste de autenticação com Streamlit')
